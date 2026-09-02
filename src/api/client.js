1d0ad12b1f682b2d46e7412f2fd59c8452d13e81
neddmont <vovakrasivih@gmail.com>
2026-09-02 08:03:54 +0300

    fsdfs

--- src/api/client.js
+++ src/api/client.js
@@ -6,7 +6,7 @@ const API_URL = 'https://tasklist-backend-j30i.onrender.com';
 export const api = axios.create({
     baseURL: API_URL,
     headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
     },
 });
 
