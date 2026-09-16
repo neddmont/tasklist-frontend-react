d4d23dc5d00125347bf7a7e35b4c321ceebe7639
neddmont <vovakrasivih@gmail.com>
2026-09-16 10:34:38 +0300

    Изменен апи

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://tasklist-backend-j30i.onrender.com';
+const API_URL = 'https://tasklist-backend-tgxv.onrender.com';
 
 
 export const api = axios.create({
--- src/components/tasks/CreateTaskForm.jsx
+++ src/components/tasks/CreateTaskForm.jsx
@@ -1,10 +1,18 @@
+import {useState } from 'react';
 import Button from '../ui/Button';
 import Input from '../ui/Input';
 
-const CreateTaskForm = ({ value, onChange, onCreate }) => {
+const CreateTaskForm = ({ onCreate }) => {
+    const [title, setTitle] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        onCreate();
+        if (!title.trim()) {
+            alert('Название задачи не может быть пустым');
+            return;
+        }
+        onCreate(title);
+        setTitle('');
     };
 
     return (
@@ -12,8 +20,8 @@ const CreateTaskForm = ({ value, onChange, onCreate }) => {
             <Input
                 type="text"
                 placeholder="Название задачи"
-                value={value}
-                onChange={(e) => onChange(e.target.value)}
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
                 required
             />
             <Button type="submit" variant="primary">
